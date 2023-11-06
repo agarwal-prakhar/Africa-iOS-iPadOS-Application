c288782194f8fa8e8c6a232b4fe9d6b424c1204a
Prakhar Agarwal <realprakhar@outlook.com>
2023-11-06 23:07:44 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Prakhar Agarwal on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
