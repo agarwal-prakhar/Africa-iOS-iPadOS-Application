051c2f47765991142e50536a47cd8e133a97ec12
Prakhar Agarwal <realprakhar@outlook.com>
2023-11-15 20:18:35 +0530

    GridViewLayout
    GridViewLayout

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -16,10 +16,30 @@ struct ContentView: View {
     let haptics = UIImpactFeedbackGenerator(style: .medium)
     
     @State private var isGridViewActive: Bool = false
+    @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+  //  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    // MARK: - FUNCTION
     
-    
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
+        
+        //: TOOLBAR IMAGE
+        switch gridColumn{
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
     var body: some View {
      
         //MARK: BODY
@@ -71,8 +91,9 @@ struct ContentView: View {
                                 print("Grid view is activated")
                                 isGridViewActive = true
                                 haptics.impactOccurred()
+                                gridSwitch()
                             }){
-                                Image(systemName: "square.grid.2x2")
+                                Image(systemName: toolbarIcon)
                                     .font(.title2)
                                     .foregroundColor(isGridViewActive ? .accentColor : .primary)
                             }
